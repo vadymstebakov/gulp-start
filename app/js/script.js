7c1e98941da5bee3d6f2c860a38fb96e1e79beeb
Vadym <vadim.stebakov.94@gmail.com>
2018-09-01 17:11:33 +0300

    template gulp-start

---
+++ app/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // Resize function
+    function onResize() {
+        console.log('e');
+    };
+    var doit;
+    doit = setTimeout(onResize, 400);
+    window.onresize = function() {
+        clearTimeout(doit);
+        doit = setTimeout(onResize, 400);
+    };
+
+})
